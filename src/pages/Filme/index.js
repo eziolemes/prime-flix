b873cc849a75a2a92a3aba8074963270e36403c1
Ezio Lemes <ti@ciclocairu.com.br>
2022-12-23 17:08:34 -0400

    add estilos na pagina de detalhes do filme

--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -1,8 +1,62 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import './filme-info.css';
+import api from '../../services/api';
 
 function Filme() {
+  const { id } = useParams();
+  const [filme, setFilme] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function loadFilme() {
+      await api.get(`/movie/${id}`, {
+        params: {
+          api_key: "5bc3301c5424e2acbece33016143080b",
+          language: "pt-BR",
+        }
+      })
+      .then((response) => {
+        setFilme(response.data);
+        setLoading(false);
+      })
+      .catch(() => {
+        console.log("não encontrado");
+      })
+    }
+
+    loadFilme();
+
+    return () => {
+      console.log('componente foi desmontado')
+    }
+  }, []);
+
+  if (loading) {
+    return(
+      <div className="filme-info">
+        <h1>Carregando detalhes...</h1>
+      </div>
+    )
+  }
+
   return(
-    <div>
-      <h1>Bem-vindo a pagina detalhes do filme</h1>
+    <div className="filme-info">
+      <h1>{filme.title}</h1>
+      <img src={`http://image.tmdb.org/t/p/original/${filme.backdrop_path}`} alt={filme.title} />
+
+      <h3>Sinopse</h3>
+      <span>{filme.overview}</span>
+      <strong>Avaliação: {filme.vote_average} / 10</strong>
+
+      <div className="area-buttons">
+        <button>Salvar</button>
+        <button>
+          <a href="#">
+            Trailer
+          </a>
+        </button>
+      </div>
     </div>
   )
 }
