437c113846413c76b1c7fd50ccc2da31c28a30be
Ezio Lemes <ti@ciclocairu.com.br>
2022-12-23 18:14:37 -0400

    add pagina favoritos

--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -35,6 +35,24 @@ function Filme() {
     }
   }, [navigate, id]);
 
+  function salvarFilme() {
+    const minhaLista = localStorage.getItem("@primeflix");
+
+    let filmesSalvos = JSON.parse(minhaLista) || [];
+
+    const hasFilme = filmesSalvos.some((filmeSalvo) => filmeSalvo.id === filme.id)
+
+    if (hasFilme) {
+      alert("Esse filme já esta na lista");
+      return;
+    }
+
+    filmesSalvos.push(filme);
+    localStorage.setItem("@primeflix", JSON.stringify(filmesSalvos));
+    alert('Filme salvo com sucesso!');
+
+  }
+
   if (loading) {
     return(
       <div className="filme-info">
@@ -53,9 +71,9 @@ function Filme() {
       <strong>Avaliação: {filme.vote_average} / 10</strong>
 
       <div className="area-buttons">
-        <button>Salvar</button>
+        <button onClick={salvarFilme}>Salvar</button>
         <button>
-          <a target="_blank" rel="external noreferrer" href={`https://youtube.com/results?search_query=${filme.title} Trailer`}>
+          <a target="blank" rel="external noreferrer" href={`https://youtube.com/results?search_query=${filme.title} Trailer`}>
             Trailer
           </a>
         </button>
--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import Filme from './pages/Filme';
 
 import Header from './components/Header';
 import Erro from './pages/Erro';
+import Favoritos from './pages/Favoritos';
 
 function RoutesApp() {
   return(
@@ -13,6 +14,7 @@ function RoutesApp() {
       <Routes>
         <Route path="/"          element={ <Home/> } />
         <Route path="/filme/:id" element={ <Filme/> } />
+        <Route path="/favoritos" element={ <Favoritos/> } />
 
         <Route path="*"          element={ <Erro/> } />
       </Routes>
