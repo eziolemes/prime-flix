fa735bb93663331f75e61987dcb93ebf3cea6510
Ezio Lemes <ti@ciclocairu.com.br>
2022-12-23 15:02:52 -0400

    listando filmes

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react';
 import api from '../../services/api';
+import { Link } from 'react-router-dom';
+import './home.css';
 
 
 function Home() {
@@ -16,7 +18,8 @@ function Home() {
         }
       })
 
-      console.log(response.data.results)
+      //console.log(response.data.results.slice(0, 10))
+      setFilmes(response.data.results.slice(0, 10));
     }
 
     loadFilmes();
@@ -24,8 +27,18 @@ function Home() {
   });
 
   return(
-    <div>
-      <h1>Bem-vindo a home</h1>
+    <div className='container'>
+      <div className="lista-filmes">
+        {filmes.map((filme) => {
+          return(
+            <article key={filme.id}>
+              <strong>{filme.title}</strong>
+              <img src={`http://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title} />
+              <Link to={`/filme/${filme.id}`}>Acessar</Link>
+            </article>
+          )
+        })}
+      </div>
     </div>
   )
 }
