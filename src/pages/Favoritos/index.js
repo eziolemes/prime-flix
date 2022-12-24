3ba8bb7f18c2a8a98057932785fd7e73b281517e
Ezio Lemes <eziolemes@gmail.com>
2022-12-24 15:09:06 -0400

    add ação excluir filme

--- src/pages/Favoritos/index.js
+++ src/pages/Favoritos/index.js
@@ -11,9 +11,20 @@ function Favoritos(){
     setFilmes(JSON.parse(minhaLista) || []);
   }, [])
 
+  function excluirFilme(id) {
+    let filtroFilmes = filmes.filter((item) => {
+      return (item.id != id);
+    })
+
+    setFilmes(filtroFilmes);
+    localStorage.setItem("@primeflix", JSON.stringify(filtroFilmes));
+  }
+
   return(
     <div className="meus-filmes">
       <h1>Meus filmes</h1>
+
+      {filmes.length === 0 && <span>Você não possui nenhum filme salvo :(</span>}
 
       <ul>
         {filmes.map((item) => {
@@ -22,7 +33,7 @@ function Favoritos(){
               <span>{item.title}</span>
               <div>
                 <Link to={`/filme/${item.id}`}>Ver detalhes</Link>
-                <button>Excluir</button>
+                <button onClick={() => excluirFilme(item.id)}>Excluir</button>
               </div>
             </li>
           )
